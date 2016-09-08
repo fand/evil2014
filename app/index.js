990fc8832fd5fe0fd137171c00ecb3719c216342
amagitakayosi <fand.gmork@gmail.com>
2016-09-08 18:12:40 +0900

    serve favicon

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const morgan  = require('morgan');
 const path    = require('path');
 const ect     = require('ect');
 const ectRenderer = ect({ watch: true, root: __dirname + '/views', ext : '.ect' });
-
+const favicon = require('serve-favicon');
 
 const routes   = require('./routes');
 
@@ -13,6 +13,7 @@ app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ect');
 app.engine('ect', ectRenderer.render);
 app.use('/static', express.static('static'));
+app.use(favicon(__dirname + '/../static/favicon.ico'));
 
 app.get('/mobile', routes.mobile);
 app.get('/', routes.top);
